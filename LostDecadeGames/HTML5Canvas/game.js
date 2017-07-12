dd0800c562af2c091aa8a5ce9cea1b23eb502a76
Adam Lombard <willcodeforgin@gmail.com>
2017-07-12 09:56:39 -0700

    adding images

--- LostDecadeGames/HTML5Canvas/game.js
+++ LostDecadeGames/HTML5Canvas/game.js
@@ -6,3 +6,14 @@ canvas.width = 512;
 canvas.height = 480;
 
 document.body.appendChild(canvas);
+
+
+// Background image
+var backgroundReady = false;
+var backgroundImage = new Image();
+
+backgroundImage.onload = function () {
+  backgroundReady = true;
+}
+
+backgroundImage.src = "images/background.png"
