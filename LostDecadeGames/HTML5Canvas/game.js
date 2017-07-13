87b66dffd5df3a7affc224c4b5b9d5aab7592617
Adam Lombard <willcodeforgin@gmail.com>
2017-07-13 08:23:19 -0700

    update game objects

--- LostDecadeGames/HTML5Canvas/game.js
+++ LostDecadeGames/HTML5Canvas/game.js
@@ -70,3 +70,31 @@ var reset = function () {
   monster.x = 32 + (Math.random() * (canvas.width - 64));
   monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
+
+// Update game objects
+var update = function (modifier) {
+  if (38 in keysDown) { // Up
+    hero.y -= hero.speed * modifier;
+  }
+  if (40 in keysDown) { // Down
+    hero.y += hero.speed * modifier;
+  }
+  if (37 in keysDown) { // Left
+    hero.x -= hero.speed * modifier;
+  }
+  if (39 in keysDown) { // Right
+    hero.x += hero.speed * modifier;
+  }
+
+  // Are the characters touching?
+  if (
+    hero.x <= (monster.x + 32)
+    && monster.x <= (hero.x + 32)
+    && hero.y <= (monster.y + 32)
+    && monster.y <= (hero.y + 32)
+  ) {
+    ++monstersCaught;
+    reset();
+  }
+};
+
