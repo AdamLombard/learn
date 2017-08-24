e8f27050d5cdc096319f06d7f2574a030a008a65
Adam Lombard <AdamLombard@users.noreply.github.com>
2017-08-24 16:24:08 -0700

    playing with immediately invoked functions

--- lynda/js-essential-training/script.js
+++ lynda/js-essential-training/script.js
@@ -1,7 +1,7 @@
-var theBiggest = function(a, b) {
+var theBiggest = (function(a, b) {
   return a > b ?
     ['a', a]:
     ['b', b];
-}
+})(7/9, 13/25)
 
-console.log(theBiggest(7/9, 13/25));
+console.log(theBiggest);
