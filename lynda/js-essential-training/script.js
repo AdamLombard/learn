2e01c05bf8a9e957fbcf8e205b0be977173aafbc
Adam Lombard <willcodeforgin@gmail.com>
2017-08-25 10:13:58 -0700

    accepting changes

--- lynda/js-essential-training/script.js
+++ lynda/js-essential-training/script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-function findBiggestFraction(a, b) {
-  a > b ?
-    console.log("a: ", a):
-    console.log("b: ", b);
-}
-
-var firstFraction = 3/4;
-var secondFraction = 5/7;
-
-findBiggestFraction(firstFraction, secondFraction);
-findBiggestFraction(7/16, 13/25);
-findBiggestFraction(1/2, 3/4);
-=======
 function Course(title, instructor, level, published, views) {
   this.title = title;
   this.instructor = instructor;
@@ -29,4 +15,3 @@ var courses = [
 ]
 
 console.log(courses[0].instructor);
->>>>>>> 2d7ba3aa45ba1e5432ea4e8fc3bf5395e51b08ff
