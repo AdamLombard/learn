4c42fbb19a38e5603106b70f54fbc654a86f87ee
Adam Lombard <willcodeforgin@gmail.com>
2017-09-06 11:19:33 -0700

    playing with watchers

--- vuejsGuide/scripts/main.js
+++ vuejsGuide/scripts/main.js
@@ -6,12 +6,40 @@ var vm = new Vue({
   el: "#app",
 
   data: {
-    message: 'Hello from Vue!'
+    question: '',
+    answer: 'I cannot answer until you ask!',
+    imgSrc: ''
+  },
+
+  watch: {
+    question: function (newQuestion) {
+      this.answer = 'Waiting for the typing to stop...',
+      this.getAnswer()
+    }
+  },
+
+  methods: {
+    getAnswer: _.debounce(
+      function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = "I don't see a question mark...";
+          return
+        }
+        this.answer = 'Thinking...'
+        var newVm = this;
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            newVm.answer = '';
+            newVm.imgSrc = response.data.image;
+          })
+          .catch(function (error) {
+            newVm.answer = 'Error! ' + error;
+          })
+      },
+      500
+    )
   },
 
   computed: {
-    reverseMessage: function () {
-      return this.message.split('').reverse().join('');
-    }
   }
 });
